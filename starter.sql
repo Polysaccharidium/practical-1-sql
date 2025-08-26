51e3c79b42af927e48d679257e027187003cbfc6
Polysaccharidium <polymaxob@gmail.com>
2025-08-26 14:51:48 +0300

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,5 +1,5 @@
--- В этом файле вы пишете свои SQL-запросы
--- Например:
+-- Обновляем студента с id=1
+UPDATE students SET name='Alice_updated', age=22 WHERE id=1;
 
--- UPDATE students SET name='Alice_updated', age=22 WHERE id=1;
--- DELETE FROM students WHERE id=2;
+-- Удаляем студента с id=2
+DELETE FROM students WHERE id=2;
